739cd86eb4c1504cc4a3763f4b16b2d4286d9400
htanivar <37423501+htanivar@users.noreply.github.com>
2025-01-05 23:56:19 +0530

    High-order funtions example added (#6)

--- execute_tests.bat
+++ execute_tests.bat
@@ -2,3 +2,9 @@ go test ./... -v
 
 @REM execute all the tests without gettig it from cache
 @REM go test -count=1 ./...
+
+@REM go test ./... -coverprofile=coverage.out
+@REM go tool cover -html=coverage.out
+
+@REM go test -cover ./functions
+@REM golangci-lint run
--- functions/01-normal/normal_test.go
+++ functions/01-normal/normal_test.go
@@ -25,3 +25,65 @@ func TestAdd(t *testing.T) {
 	}
 
 }
+
+func TestMinus(t *testing.T) {
+	tests := []struct {
+		name     string
+		a, b     int
+		expected int
+	}{
+		{"Minus negative numbers", -2, -3, 1},
+		{"Minus Positive & Negative Numbers", 10, -12, 22},
+		{"Minus Zeros", 0, 0, 0},
+		{"Minus Large Numbers", 1000000, 2000000, -1000000},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := normal_minus(tt.a, tt.b); got != tt.expected {
+				t.Errorf("normal_minus() = %v, want %v", got, tt.expected)
+			}
+		})
+	}
+}
+
+func TestTimes(t *testing.T) {
+	tests := []struct {
+		name string
+		a, b int
+		want int
+	}{
+		{"Times positive numbers", 2, 3, 6},
+		{"Times negative numbers", -2, -3, 6},
+		{"Times Positive & Negative Numbers", 10, -12, -120},
+		{"Times Zeros", 0, 0, 0},
+		{"Times Large Numbers", 1000000, 2000000, 2000000000000},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := normal_times(tt.a, tt.b); got != tt.want {
+				t.Errorf("normal_times() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDivision(t *testing.T) {
+	tests := []struct {
+		name string
+		a, b float64
+		want float64
+	}{
+		{"Divide positive numbers", 2, 3, 0.67},
+		{"Divide negative numbers", -2, -3, 0.67},
+		{"Divide Positive & Negative Numbers", 10, -12, -0.83},
+		{"Divide Zeros", 0, 0, 0},
+		{"Divide Large Numbers", 1000000, 2000000, 0.5},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := normal_division(tt.a, tt.b); got != tt.want {
+				t.Errorf("normal_division() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
